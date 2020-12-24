179f9ce602566d80b9c4ed247165b59e661be718
Diego Gonzalez <dyjps2012@gmail.com>
2020-12-24 02:57:07 -0300

    install necessary libraries for react app spa

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
+mix.react('resources/js/index.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.browserSync({
+    proxy: 'http://mpv-clinica-dental.test/',
+    open: false
+});
