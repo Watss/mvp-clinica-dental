2cee7b79d26a249807abc94a0fb1202edee9acde
Diego Gonzalez <dyjps2012@gmail.com>
2020-12-25 17:50:26 -0300

    initialize config mixin apibuilder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Api\ApiBuilder;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Builder::mixin(new ApiBuilder);
     }
 }
