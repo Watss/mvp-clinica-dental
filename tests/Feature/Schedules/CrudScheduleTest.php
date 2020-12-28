980814d22e05218c1539c1d236f4914d41bc421c
Diego Gonzalez <dyjps2012@gmail.com>
2020-12-28 15:15:25 -0300

    created test file and implement testing store schedule

---
+++ tests/Feature/Schedules/CrudScheduleTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Schedules;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CrudScheduleTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+        @test
+     */
+    public function can_create_schedule_and_details()
+    {
+        
+        $scheduleData = [
+            "name" =>  "calendario personalizado",
+            "schedule_details" => [
+                [
+                    'day_name' => 'Lunes',
+                    'day_number' => '1',
+                    'start_time' => '08:00:00',
+                    'end_time' => '19:00:00',
+                    'rest_start_time' => '13:00:00',
+                    'rest_end_time' => '14:00:00',
+                ],
+                [
+                    'day_name' => 'Martes',
+                    'day_number' => '2',
+                    'start_time' => '08:00:00',
+                    'end_time' => '19:00:00',
+                    'rest_start_time' => '13:00:00',
+                    'rest_end_time' => '14:00:00',
+                ],
+                [
+                    'day_name' => 'Miercoles',
+                    'day_number' => '3',
+                    'start_time' => '08:00:00',
+                    'end_time' => '19:00:00',
+                    'rest_start_time' => '13:00:00',
+                    'rest_end_time' => '14:00:00',
+                ]
+            ]
+        ];
+
+        $response = $this->postJson(route('schedule.store'),$scheduleData);
+        
+        $response->assertStatus(201);
+        
+        $response->assertJsonStructure([
+            'success',
+            'schedule' => [
+                'type',
+                'id',
+                'attributes' => [
+                    'name',
+                    'details',
+                    'created_at',
+                    'updated_at'
+                ],
+                'links' => []
+            ]
+        ]);
+
+    }
+}
