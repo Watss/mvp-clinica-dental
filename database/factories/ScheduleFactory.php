945e56d997723afb14720c6e5c881fd8d90bf902
Diego Gonzalez <dyjps2012@gmail.com>
2020-12-28 15:14:17 -0300

    added schedule factory

---
+++ database/factories/ScheduleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Schedule;
+use Faker\Generator as Faker;
+
+$factory->define(Schedule::class, function (Faker $faker) {
+    return [
+        'name' => "calendario $faker->word"
+    ];
+});
