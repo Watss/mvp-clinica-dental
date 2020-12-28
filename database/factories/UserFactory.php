d59d910617a27160a7c74d7c4308e209576c4303
Diego Gonzalez <dyjps2012@gmail.com>
2020-12-28 11:45:30 -0300

    modify user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,6 +26,6 @@ $factory->define(User::class, function (Faker $faker) {
         'adress' => $faker->address,
         'phone_number' => $faker->phoneNumber,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'office_id' => factory(App\Models\Office::class)
+        'office_id' => 1
     ];
 });
