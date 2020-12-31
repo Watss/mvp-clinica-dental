7e359786f90a476b80000992a7bcee92683fc0b7
Abel <eliasvalenzuela165@gmail.com>
2020-12-31 07:39:07 -0300

    created form create dentist

---
+++ resources/js/utils/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const axiosInstance = axios.create({
+    baseURL: 'http://mvp-clinica-dental.test/api/v1/',
+    timeout: 1000,
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/vnd.api+json'
+    }
+});
+
+export default axiosInstance
